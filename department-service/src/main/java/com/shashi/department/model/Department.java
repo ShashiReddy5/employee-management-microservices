33de0c33975c4bcd5912dddfd7802a6d0c2e7821
Shashidhar Reddy <shashirdy74@gmail.com>
2026-07-09 10:13:37 -0400

    Create Department.java

---
+++ department-service/src/main/java/com/shashi/department/model/Department.java
@@ -0,0 +1,26 @@
+package com.shashi.department.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String location;
+}
