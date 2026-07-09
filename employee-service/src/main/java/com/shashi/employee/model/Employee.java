b0bf9c8d4cac7d76850b1b09f89f85d7389b6987
Shashidhar Reddy <shashirdy74@gmail.com>
2026-07-09 10:09:45 -0400

    Create Employee.java

---
+++ employee-service/src/main/java/com/shashi/employee/model/Employee.java
@@ -0,0 +1,35 @@
+package com.shashi.employee.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String jobTitle;
+
+    @Column(nullable = false)
+    private Long departmentId;
+
+    @Column(nullable = false)
+    private Double annualSalary;
+}
